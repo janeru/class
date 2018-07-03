b42d637079d7647d2d41354bb4310f5f1512c4f6
RU <b03801053@ntu.edu.tw>
2018-07-04 00:13:12 +0800

    add dropdown component

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,18 @@ import { connect } from "react-redux";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ClassAdmin from './ClassAdmin';
 import Class from '../actions/Class'
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { addClass } from '../actions/Class'
 import { deleteClass } from '../actions/Class'
-// import uuidv1 from "uuid";
 import { Container, Row, Col, Jumbotron, Button, Card, CardImg, CardBlock, CardTitle, CardSubtitle, CardText, Badge, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 const mapStateToProps = (state) => {
-  // console.log(state)
+  console.log(state.datas.data)
   return {
     data: state.datas.data,
   }
-}
 
+}
 
 class ClassPage extends Component {
   constructor(props) {
@@ -24,12 +23,25 @@ class ClassPage extends Component {
       modal: false,
       name: '',
       studentMaxNum: 0,
-
+      classAdminPage: false,
+      classId: NaN
     };
   }
   //控制modal的彈跳
   toggle = () => {
     this.setState(({ modal }) => ({ modal: !modal }));
+  }
+
+  //控制班級頁面是否出現，哪個班級的資料跟index也傳進來
+  classAdminFunc = (data, index) => () => {
+    // console.log(data)
+    // console.log(index)
+    this.setState(({ classAdminPage, classId }) => ({
+      classAdminPage: !classAdminPage,
+      classId: data.id
+    }))
+    // console.log(this.state.classAdminPage)
+    // console.log(this.state.classId)
   }
 
   handleChange = (event) => {
@@ -43,70 +55,80 @@ class ClassPage extends Component {
     this.setState(({ name, studentMaxNum, modal }) => ({ name: "", studentMaxNum: 0, modal: !modal }))
   }
 
+
   render() {
-    const { modal } = this.state
+    const { modal, classAdminPage, classId } = this.state
     return (
       <div>
+        {classAdminPage === true ? (<ClassAdmin class_Id={classId} classAdmin_Page={classAdminPage} />) :
+          (
+            <div>
+              <Button color="secondary" onClick={this.toggle}>新增班級</Button>
 
-        <Button color="secondary" onClick={this.toggle}>新增班級</Button>
-        {
-          this.props.data.map((data, index) => (
-            <Row className="photo">
-              <Col sm={6} md={4} className="mb-3">
-                <Card className="content">
-                  <Link to="/create">
-                    <CardImg width="100%" src="https://picsum.photos/600/400?image=1067" alt="Card image cap" />
-                  </Link>
-                  <i class="fas fa-trash" onClick={() => this.props.dispatch(deleteClass({ id: data.id }))} />
-                  <CardBlock>
-                    <CardTitle>
-                      {data.name}
-                      {data.id}
-                    </CardTitle>
-                    <CardText>
-                      {data.studentsNum != undefined ? data.studentsNum + "/" + data.studentMaxNum : 0 + "/" + data.studentMaxNum}
-                    </CardText>
-                  </CardBlock>
-                </Card>
-              </Col>
-            </Row>
+              <div>
+                {this.props.data.map((data, index) => (
 
-          ))
+                  <Row className="photo">
+                    <Col sm={6} md={4} className="mb-3">
+                      <Card className="content" >
+                        <CardImg width="100%" src="https://picsum.photos/800/900?image=1067" alt="Card image cap"
+                          onClick={this.classAdminFunc(data, index)}
+                        />
+                        <i class="fas fa-trash" onClick={() => this.props.dispatch(deleteClass({ id: data.id }))} />
+                        <CardBlock>
+                          <CardTitle>
+                            {data.name}
+                            {/* {data.id} */}
+                          </CardTitle>
+                          <CardText>
+                            {data.studentsNum !== undefined ? data.studentsNum + "/" + data.studentMaxNum : 0 + "/" + data.studentMaxNum}
+                          </CardText>
+                        </CardBlock>
+                      </Card>
+                    </Col>
+                  </Row>
+
+                ))
+                }
+              </div>
+
+              {/* 新增後所彈跳出來的Modal */}
+              <Modal isOpen={modal} toggle={this.toggle}>
+                <ModalHeader toggle={this.toggle}>新增班級</ModalHeader>
+                <ModalBody>
+                  <Form>
+                    <FormGroup>
+                      <Label for="name">班級名稱</Label>
+                      <Input type="text" name="classname" id="name" placeholder=""
+
+                        onChange={
+                          (input) => this.handleChange(input)
+                        }
+                      />
+                      <Label for="studentMaxNum">人數</Label>
+                      <Input type="number" name="classpeoplenum" id="studentMaxNum" placeholder=""
+                        onChange={
+                          (input) => this.handleChange(input)
+                        }
+                      />
+                    </FormGroup>
+                  </Form>
+                </ModalBody>
+                <ModalFooter>
+                  <Button onClick={this.handleSubmit}>
+                    儲存
+                </Button>
+                </ModalFooter>
+              </Modal>
+
+            </div>
+          )
 
         }
-        {/* 新增後所彈跳出來的Modal */}
-        <Modal isOpen={modal} toggle={this.toggle}>
-          <ModalHeader toggle={this.toggle}>新增班級</ModalHeader>
-          <ModalBody>
-            <Form>
-              <FormGroup>
-                <Label for="name">班級名稱</Label>
-                <Input type="text" name="classname" id="name" placeholder=""
-
-                  onChange={
-                    (input) => this.handleChange(input)
-                  }
-                />
-                <Label for="studentMaxNum">人數</Label>
-                <Input type="number" name="classpeoplenum" id="studentMaxNum" placeholder=""
-                  onChange={
-                    (input) => this.handleChange(input)
-                  }
-                />
-              </FormGroup>
-            </Form>
-          </ModalBody>
-          <ModalFooter>
-            <Button onClick={this.handleSubmit}>
-              儲存
-                </Button>
-          </ModalFooter>
-        </Modal>
       </div>
-    );
+    )
   }
+
 }
-
-
 const App = connect(mapStateToProps)(ClassPage);
 export default App;
