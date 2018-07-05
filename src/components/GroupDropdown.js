8bf7dd1ad0bbf09af76cb6db9590bc294130274d
RU <b03801053@ntu.edu.tw>
2018-07-05 14:16:57 +0800

    Alter group list and css

--- src/components/GroupDropdown.js
+++ src/components/GroupDropdown.js
@@ -24,17 +24,35 @@ class GroupSheet extends Component {
         this.state = {
             //控制下拉式選單
             dropdownOpen: false,
-
+            dropDownValue: '選單',
+            buttonToChoose: NaN,
+            nowClickGroupId: NaN
         };
+
     }
 
     //控制下拉式選單
     toggleSheet = () => {
         this.setState(({ dropdownOpen }) => ({ dropdownOpen: !dropdownOpen }));
     }
+
+    changeValue = (name, id, number) => () => {
+        this.setState(({ dropDownValue, buttonToChoose, nowClickGroupId }) => ({ dropDownValue: name, nowClickGroupId: id, buttonToChoose: number }))
+    }
+    // whichGroup = (clickGroupId) => () => {
+    //     this.setState(({ nowClickGroupId }) => ({ nowClickGroupId: clickGroupId }))
+    // }
+
     render() {
         const { nowClass } = this.props
-        const { modal } = this.state
+        const { modal, buttonToChoose, nowClickGroupId } = this.state
+        const whole = '全部'
+        const noGroup = '未分組'
+        const numberWhole = 0
+        const numberGroup = 1
+        const numberNoGroup = 2
+
+
 
         return (
             <div>
@@ -44,44 +62,96 @@ class GroupSheet extends Component {
                 <Col className="dropdownRow">
 
                     <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggleSheet}>
-                        <DropdownToggle caret color="warning" className="dropdownMenu">
-                            全部
-
+                        <DropdownToggle caret color="warning" className="dropdownMenu" >
+                            {this.state.dropDownValue}
                         </DropdownToggle>
 
-                        <DropdownMenu>
-                            {nowClass.groups.length === 0 ? (''
+                        <DropdownMenu >
+                            <div onClick={this.changeValue(whole, '', numberWhole)}>全部</div>
 
-                            ) : (
-                                    <div>
-                                        {//顯示是哪一個組別以及組員有誰
-                                            nowClass.groups.map((group) => {
-                                                return (
-                                                    <div>
+                            {nowClass.groups.map((group) =>
+                                <div onClick={this.changeValue(group.name, group.id, numberGroup)}>{group.name}</div>)
+                            }
+                            <div onClick={this.changeValue(noGroup, '', numberNoGroup)}>未分組</div>
+                        </DropdownMenu>
+                        {/* 如果點到全部，有正常名字的都要顯示出來 */}
+                        <div className="clickGroupStuentsItem">
+                            {(buttonToChoose === 0) ? (
 
-                                                        <div>
-                                                            {group.name}
-                                                        </div>
-                                                        <div>
-                                                            {group.students.map((student) => {
-                                                                return student.name === '尚未加入' ? ('') : (
-                                                                    <DropdownItem>{student.name}</DropdownItem>)
-                                                            })}
-                                                        </div>
-                                                    </div>
+
+                                <DropdownMenu id="Item">
+                                    <div >
+
+                                        <div>
+
+                                            {nowClass.groups.map((group) =>
+
+                                                (group.students.map((student) => {
+                                                    if (student.name !== '尚未加入') {
+                                                        return <DropdownItem disabled>{student.name}</DropdownItem>
+                                                    }
+                                                })
                                                 )
-                                            })}
-                                        <div>未分組</div>
+                                            )
+                                            }
+                                        </div>
+
                                         <div>
                                             {nowClass.students.map((student) => {
                                                 if (student.name !== '尚未加入') {
-                                                    return <DropdownItem>{student.name}</DropdownItem>
+                                                    console.log(student.name)
+                                                    return <DropdownItem disabled>{student.name}</DropdownItem>
                                                 }
                                             })}
                                         </div>
-                                    </div>)
-                            }
-                        </DropdownMenu>
+                                    </div>
+                                </DropdownMenu>
+                            ) : ('')}
+
+                            {/* 先確認是點到組別，在確定是點到哪一個組別，並顯示該組別的學生名字 */}
+                            {(buttonToChoose === 1) ? (
+                                nowClass.groups.length === 0 ? (''
+
+                                ) : (
+                                        <DropdownMenu id="Item">
+                                            <div >
+                                                {/* 顯示是哪一個組別以及組員有誰 */}
+                                                {nowClass.groups.map((group) =>
+                                                    (group.id === nowClickGroupId) ?
+
+                                                        (group.students.map((student) => {
+
+                                                            if (student.name !== '尚未加入') {
+                                                                return <DropdownItem disabled>{student.name}</DropdownItem>
+                                                            }
+                                                        })
+                                                        )
+                                                        : ('')
+
+                                                )
+                                                }
+                                            </div>
+                                        </DropdownMenu>
+                                    )
+                            ) : ('')}
+
+                            {/* 如果點到沒有組別，那麼就要顯示沒有組別的名字 */}
+                            {(buttonToChoose === 2) ? (
+
+                                <DropdownMenu id="Item">
+                                    <div >
+                                        {console.log(nowClass.groups.length)}
+                                        {nowClass.students.map((student) => {
+                                            if (student.name !== '尚未加入') {
+                                                console.log(student.name)
+                                                return <DropdownItem disabled>{student.name}</DropdownItem>
+                                            }
+                                        })}
+                                    </div>
+                                </DropdownMenu>)
+                                : ('')}
+
+                        </div>
                     </ButtonDropdown>
 
                 </Col>
